430fff2bd88f1b2e5f55e8fdacda8e5fac314331
GraceGnanamJ-1053 <gracegnanam@karunya.edu.in>
2025-04-18 21:26:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 //connect to MongoDB
-mongoose.connect('mongodb+srv://gracegnanam:HackerGrace#4@inventory.iziargj.mongodb.net/?retryWrites=true&w=majority&appName=Inventory')
+mongoose.connect('mongodb+srv://gracegnanam:123@inventory.iziargj.mongodb.net/?retryWrites=true&w=majority&appName=Inventory')
 .then(() => console.log('DB connected'))
 .catch(err => console.log(err))
 
